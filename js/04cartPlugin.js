72638d9748e4618003ec7bded3c861c349bf4249
ASroshdy95 <ahmedsaeedroshdy@gmail.com>
2021-05-25 02:52:25 +0200

    first commit

---
+++ js/04cartPlugin.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $(".cart-page table:first-child p.remove-prod").click(function(){
+        
+        $(this).parent(".info").parent("td").parent("tr").remove();
+        
+        });
+    
+    
+
+    
+    
+    });
+    
